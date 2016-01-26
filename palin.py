2b2e4c2297617fda55cf2a4e9d6ef9c81c473879
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2016-01-26 05:24:00 -0800

    26 Jan: skeleton Git repo

---
+++ palin.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A docstring goes here, you know"""
+
+import sentence_generator       # https://github.com/patrick-brian-mooney/markov-sentence-generator
+
+if __name__ == "__main__":
+    pass
