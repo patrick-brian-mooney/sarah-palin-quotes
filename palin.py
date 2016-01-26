fcb89540194cb062f0f488487d23354656a387e1
Patrick Brian Mooney <git.pmooney78@neverbox.com>
2016-01-26 12:25:22 -0800

    26 Jan 2016: Basically functional skeleton.

--- palin.py
+++ palin.py
@@ -1,7 +1,15 @@
-#!/usr/bin/env python3
-"""A docstring goes here, you know"""
+#!/usr/bin/env python
 
-import sentence_generator       # https://github.com/patrick-brian-mooney/markov-sentence-generator
+from sentence_generator import *       # https://github.com/patrick-brian-mooney/markov-sentence-generator
+import glob
 
-if __name__ == "__main__":
-    pass
+print('Content-Type: text/html; charset=utf-8\n\n')
+print('<title>Sarah Palin Gibberish</title>')
+print('<h1>Sarah Palin Quote, or Random Gibberish?</h1>')
+
+the_list = []
+for which_file in glob.glob('speeches/*txt'):
+    the_list = the_list + word_list(which_file)
+starts, the_mapping = buildMapping(the_list, markov_length=2)
+
+print('<p>%s</p>' % gen_text(the_mapping, starts, markov_length=2, sentences_desired=5, paragraph_break_probability=0) )
